c36a2507622e948464f0cbd4cda0a6a2781c0740
Keshasan <kesha3084@gmail.com>
2021-10-16 14:12:01 +0300

    pictures added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean-folder',
     version='1.0.0',
     description='Script sorting tree folders',
-    url='http://github.com/dummy_user/useful',
+    url='https://github.com/Keshasan/clean-folder',
     author='Aleksandr Holoborodko',
     author_email='kesha3084@gmail.com',
     license='',
